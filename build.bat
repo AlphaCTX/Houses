4ec409e4acd42fb4528be049a2a5a45a50a3d81d
AlphaCTX <154980417+AlphaCTX@users.noreply.github.com>
2025-06-25 21:25:58 +0200

    Make door command admin-only

--- build.bat
+++ build.bat
@@ -1,13 +1,17 @@
 @echo off
-if not defined MAVEN_HOME (
-    mvn -version >nul 2>&1
-    if %ERRORLEVEL% neq 0 (
-        echo Maven is not installed.
-        exit /b 1
-    )
+
+set "MAVEN_CMD=mvn"
+if defined MAVEN_HOME (
+    set "MAVEN_CMD=%MAVEN_HOME%\bin\mvn"
 )
 
-mvn clean package
+%MAVEN_CMD% -version >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo Maven is not installed or not found in PATH.
+    exit /b 1
+)
+
+%MAVEN_CMD% clean package
 if %ERRORLEVEL% neq 0 (
     echo Build failed.
     exit /b %ERRORLEVEL%
@@ -18,6 +22,6 @@ if not exist build (
 )
 
 for /f "delims=" %%i in ('dir /b target^|findstr /i "\.jar$"') do set JAR=%%i
-move /Y target\%JAR% build\%JAR%
+move /Y "target\%JAR%" "build\%JAR%"
 
 echo Build completed. Jar moved to build\%JAR%
